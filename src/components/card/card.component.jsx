165cf7be865cc4c4ed2dfe2ef3c6160e42be52b2
davi schilling <davi.bschilling@mobilesoft.it>
2020-12-04 19:02:20 +0100

    deploy

---
+++ src/components/card/card.component.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './card-styles.css'
+
+export const Card = props => (
+  <div className="card-container">
+    <img alt="user monster" src={`https://robohash.org/${props.user.id}?set=set2&size=180x250`} />
+    <h2> {props.user.name} </h2>
+    <p> {props.user.email} </p>
+  </div>
+)
